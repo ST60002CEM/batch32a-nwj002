3bcab48c636b770bdcf76f58cf6b8789ae283c44
nwj002 <nwj.shrestha@gmail.com>
2024-06-13 14:02:50 +0545

    Navigator and navigator key and constant colour theme for the application

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:liquor_ordering_system/screen/splash_screen.dart';
-import 'package:liquor_ordering_system/theme/theme_data.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: getApplicationTheme(),
-      home: const SplashScreen(),
+      home: SplashScreen(),
     );
   }
 }
