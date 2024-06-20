fda066cfbfdbbfcbbac72edb27098d2d0388a695
nwj002 <nwj.shrestha@gmail.com>
2024-06-20 14:55:05 +0545

    user regestration using api completed

--- lib/app/constants/api_endpoint.dart
+++ lib/app/constants/api_endpoint.dart
@@ -1,10 +1,10 @@
 class ApiEndpoints {
   ApiEndpoints._();
-  static const Duration connectionTimeout = Duration(seconds: 1000);
-  static const Duration receiveTimeout = Duration(seconds: 1000);
-  static const String baseUrl = "http://10.0.2.2:3000/api/";
+  static const Duration connectionTimeout = Duration(seconds: 5000);
+  static const Duration receiveTimeout = Duration(seconds: 3000);
+  static const String baseUrl = "http://192.168.1.66:5000/api/";
 
   //auth routes
   static const String login = "user/login";
-  static const String register = "user/register";
+  static const String register = "user/create";
 }
