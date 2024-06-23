5f70145d615b2b01dfcb68c3532ac496785989c6
nwj002 <nwj.shrestha@gmail.com>
2024-06-23 22:23:48 +0545

    change commection and receive timeout seconds

--- lib/app/constants/api_endpoint.dart
+++ lib/app/constants/api_endpoint.dart
@@ -1,7 +1,7 @@
 class ApiEndpoints {
   ApiEndpoints._();
-  static const Duration connectionTimeout = Duration(seconds: 5000);
-  static const Duration receiveTimeout = Duration(seconds: 3000);
+  static const Duration connectionTimeout = Duration(seconds: 1000);
+  static const Duration receiveTimeout = Duration(seconds: 1000);
   static const String baseUrl = "http://192.168.1.66:5000/api/";
 
   //auth routes
