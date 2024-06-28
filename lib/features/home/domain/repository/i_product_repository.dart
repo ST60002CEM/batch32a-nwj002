4c0fb7affe048d7eed4c002c7a53c598424bf613
nwj002 <nwj.shrestha@gmail.com>
2024-06-28 23:15:15 +0545

    product domain repositoty

---
+++ lib/features/home/domain/repository/i_product_repository.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:liquor_ordering_system/core/failure/failure.dart';
+import 'package:liquor_ordering_system/features/home/domain/entity/product_entity.dart';
+
+final productRepositoryProvider = 
+Provider<IProductRepository>((ref) => ref.read(productRepository));
+
+abstract class IProductRepository{
+  Future<Either<Failure, List<ProductEntity>>> pagination(int page, int limit);
+}
