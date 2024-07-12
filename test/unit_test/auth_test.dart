787c2f5364cdd1760c5e5d6d6714d82d01fda731
nwj002 <nwj.shrestha@gmail.com>
2024-07-12 13:23:59 +0545

    user registration testing completed

--- test/unit_test/auth_test.dart
+++ test/unit_test/auth_test.dart
@@ -2,6 +2,7 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:liquor_ordering_system/core/failure/failure.dart';
+import 'package:liquor_ordering_system/features/auth/domain/entity/auth_entity.dart';
 import 'package:liquor_ordering_system/features/auth/domain/usecases/auth_use_case.dart';
 import 'package:liquor_ordering_system/features/auth/presentation/navigator/login_navigator.dart';
 import 'package:liquor_ordering_system/features/auth/presentation/navigator/register_navigator.dart';
@@ -15,7 +16,6 @@ import 'auth_test.mocks.dart';
   MockSpec<AuthUseCase>(),
   MockSpec<LoginViewNavigator>(),
   MockSpec<RegisterViewNavigator>(),
-
 ])
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
@@ -23,7 +23,6 @@ void main() {
   late AuthUseCase mockAuthUsecase;
   late ProviderContainer container;
   late LoginViewNavigator mockLoginViewNavigator;
-
 
   setUp(
     () {
@@ -43,8 +42,7 @@ void main() {
     expect(authState.error, isNull);
   });
 
-  test('loggin test with valid username and password', () async {
-
+  test('login test with valid username and password', () async {
     //arrange
     const correctUsername = 'nwj';
     const correctPassword = 'nwj123';
@@ -67,6 +65,70 @@ void main() {
     //assert
     expect(authState.error, isNull);
   });
+
+  test(
+    'Register new user test with all details test',
+    () async {
+      // Arrange
+      when(mockAuthUsecase.registerUser(const AuthEntity(
+        fullname: 'nawaraj',
+        username: 'nwj',
+        email: 'nwj@gmail.com',
+        password: '12345678',
+        age: '18',
+      ))).thenAnswer((innovation) {
+        final auth = innovation.positionalArguments[0] as AuthEntity;
+
+        return Future.value(
+          auth.fullname.isNotEmpty &&
+                  auth.username.isNotEmpty &&
+                  auth.email.isNotEmpty &&
+                  auth.password.isNotEmpty &&
+                  auth.email.contains('@') &&
+                  auth.email.contains('.') &&
+                  auth.age.isNotEmpty
+              ? const Right(true)
+              : Left(
+                  Failure(error: 'Invalid'),
+                ),
+        );
+      });
+
+      // Act
+      await container
+          .read(authViewModelProvider.notifier)
+          .registerUser(const AuthEntity(
+            fullname: 'nawaraj',
+            username: 'nwj',
+            email: 'nwj@gmail.com',
+            password: '12345678',
+            age: '18',
+          ));
+
+      final state = container.read(authViewModelProvider);
+
+      // Assert
+      expect(state.isLoading, false);
+      expect(state.error, null);
+    },
+  );
+
+  // test("register with user credientials", () async {
+  //   when(mockAuthUsecase.registerUser(any))
+  //       .thenAnswer((_) => Future.value(const Right(true)));
+
+  //   const AuthEntity user = AuthEntity(
+  //     fullname: 'nawaraj',
+  //     username: 'nwj',
+  //     email: 'nwj@gmail.com',
+  //     password: '12345678',
+  //     age: '18',
+  //   );
+  //   await container.read(authViewModelProvider.notifier).registerUser(user);
+  //   final authState = container.read(authViewModelProvider);
+  //   // Assert
+  //   expect(authState.error, isNull);
+  // });
 
   tearDown(
     () {
