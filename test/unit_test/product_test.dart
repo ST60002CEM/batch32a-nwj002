0f8c197e62c18bb9be509fd65461d256afcf86fc
nwj002 <nwj.shrestha@gmail.com>
2024-07-12 20:19:58 +0545

    unit testing on fetching the product in dashboard set up and mock data

---
+++ test/unit_test/product_test.dart
@@ -0,0 +1,38 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:liquor_ordering_system/features/home/domain/entity/product_entity.dart';
+import 'package:liquor_ordering_system/features/home/domain/usecases/product_usecase.dart';
+import 'package:liquor_ordering_system/features/home/presentation/viewmodel/products_viewmodel.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'product_test.mocks.dart';
+import 'test_data/product_test_data.dart';
+
+@GenerateNiceMocks([
+  MockSpec<ProductUseCase>(),
+])
+void main() {
+  late MockProductUseCase mockProductUseCase;
+  late ProviderContainer container;
+  late List<ProductEntity> lstProducts;
+
+  setUp(() {
+    mockProductUseCase = MockProductUseCase();
+    lstProducts = ProductTestData.getProductsTestData();
+    container = ProviderContainer(
+      overrides: [
+        productViewModelProvider.overrideWith(
+            (ref) => ProductViewModel(productUsecase: mockProductUseCase)),
+      ],
+    );
+  });
+
+ 
+  tearDown(
+    () {
+      container.dispose();
+    },
+  );
+}
