8421e6dfedccd8de2b8baac8380dc4c473e6f806
nwj002 <nwj.shrestha@gmail.com>
2024-08-15 20:45:55 +0545

    auth dto

--- lib/features/auth/data/dto/get_current_user_dto.dart
+++ lib/features/auth/data/dto/get_current_user_dto.dart
@@ -5,36 +5,38 @@ part 'get_current_user_dto.g.dart';
 
 @JsonSerializable()
 class GetCurrentUserDto {
-  @JsonKey(name: "_id")
-  final String id;
-  final String email;
+  final String userId;
   final String fullname;
-  final String age;
+  final int phone; // Updated from String to int
   final String username;
   final String password;
+  final int age; // Updated from String to int
+  final String email;
 
   GetCurrentUserDto({
-    required this.id,
-    required this.email,
+    required this.userId,
     required this.fullname,
-    required this.age,
+    required this.phone,
     required this.username,
     required this.password,
+    required this.age,
+    required this.email,
   });
 
   AuthEntity toEntity() {
     return AuthEntity(
-      id: id,
-      email: email,
+      userId: userId,
       fullname: fullname,
-      age: age,
+      phone: phone,
       username: username,
       password: password,
+      age: age,
+      email: email,
     );
   }
 
+  Map<String, dynamic> toJson() => _$GetCurrentUserDtoToJson(this);
+
   factory GetCurrentUserDto.fromJson(Map<String, dynamic> json) =>
       _$GetCurrentUserDtoFromJson(json);
-
-  Map<String, dynamic> toJson() => _$GetCurrentUserDtoToJson(this);
 }
--- lib/features/auth/data/dto/get_current_user_dto.g.dart
+++ lib/features/auth/data/dto/get_current_user_dto.g.dart
@@ -8,20 +8,22 @@ part of 'get_current_user_dto.dart';
 
 GetCurrentUserDto _$GetCurrentUserDtoFromJson(Map<String, dynamic> json) =>
     GetCurrentUserDto(
-      id: json['_id'] as String,
-      email: json['email'] as String,
+      userId: json['userId'] as String,
       fullname: json['fullname'] as String,
-      age: json['age'] as String,
+      phone: (json['phone'] as num).toInt(),
       username: json['username'] as String,
       password: json['password'] as String,
+      age: (json['age'] as num).toInt(),
+      email: json['email'] as String,
     );
 
 Map<String, dynamic> _$GetCurrentUserDtoToJson(GetCurrentUserDto instance) =>
     <String, dynamic>{
-      '_id': instance.id,
-      'email': instance.email,
+      'userId': instance.userId,
       'fullname': instance.fullname,
-      'age': instance.age,
+      'phone': instance.phone,
       'username': instance.username,
       'password': instance.password,
+      'age': instance.age,
+      'email': instance.email,
     };
