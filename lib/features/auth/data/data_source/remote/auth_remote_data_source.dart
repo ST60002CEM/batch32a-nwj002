d6f14bf485aa3e55726d59d1eaf8e9606885120c
nwj002 <nwj.shrestha@gmail.com>
2024-08-15 20:44:48 +0545

    fetching user profile

--- lib/features/auth/data/data_source/remote/auth_remote_data_source.dart
+++ lib/features/auth/data/data_source/remote/auth_remote_data_source.dart
@@ -5,46 +5,75 @@ import 'package:liquor_ordering_system/app/constants/api_endpoint.dart';
 import 'package:liquor_ordering_system/core/failure/failure.dart';
 import 'package:liquor_ordering_system/core/networking/remote/http_service.dart';
 import 'package:liquor_ordering_system/core/shared_prefs/user_shared_prefs.dart';
+import 'package:liquor_ordering_system/features/auth/data/model/auth_api_model.dart';
 import 'package:liquor_ordering_system/features/auth/domain/entity/auth_entity.dart';
 
 final authRemoteDataSourceProvider = Provider(
   (ref) => AuthRemoteDataSource(
     dio: ref.read(httpServiceProvider),
+    authApiModel: ref.watch(authApiModelProvider),
     userSharedPrefs: ref.read(userSharedPrefsProvider),
   ),
 );
 
 class AuthRemoteDataSource {
   final Dio dio;
+  final AuthApiModel authApiModel;
   final UserSharedPrefs userSharedPrefs;
 
   AuthRemoteDataSource({
     required this.dio,
     required this.userSharedPrefs,
+    required this.authApiModel,
   });
 
-  Future<Either<Failure, bool>> registerUser(AuthEntity user) async {
+  Future<Either<Failure, bool>> registerUser(AuthEntity authEntity) async {
     try {
       Response response = await dio.post(
         ApiEndpoints.register,
-        data: {
-          "fullname": user.fullname,
-          "username": user.username,
-          "age": user.age,
-          "email": user.email,
-          "password": user.password,
-        },
+        data: authApiModel.fromEntity(authEntity).toJson(),
       );
       if (response.statusCode == 200) {
         return const Right(true);
-      } else {
-        return Left(
-          Failure(
-            error: response.data["message"],
-            statusCode: response.statusCode.toString(),
-          ),
-        );
       }
+      return Left(
+        Failure(
+          error: response.data["message"],
+          statusCode: response.statusCode.toString(),
+        ),
+      );
+    } on DioException catch (e) {
+      return Left(
+        Failure(
+          error: e.error.toString(),
+        ),
+      );
+    }
+  }
+
+  Future<Either<Failure, bool>> loginUser(
+      {required String email, required String password}) async {
+    try {
+      Response response = await dio.post(
+        ApiEndpoints.login,
+        data: {
+          "email": email,
+          "password": password,
+        },
+      );
+      if (response.statusCode == 200) {
+        final token = response.data['token'];
+        print(token);
+        await userSharedPrefs.setUserToken(token);
+
+        return const Right(true);
+      }
+      return Left(
+        Failure(
+          error: response.data["message"],
+          statusCode: response.statusCode.toString(),
+        ),
+      );
     } on DioException catch (e) {
       return Left(
         Failure(
@@ -55,27 +84,109 @@ class AuthRemoteDataSource {
     }
   }
 
-  Future<Either<Failure, bool>> loginUser(
-    String email,
-    String password,
-  ) async {
+  Future<Either<Failure, bool>> verifyUser() async {
     try {
-      Response response = await dio.post(
-        ApiEndpoints.login,
-        data: {
-          "email": email,
-          "password": password,
-        },
+      String? token;
+      final data = await userSharedPrefs.getUserToken();
+      data.fold(
+        (l) => token = null,
+        (r) => token = r,
+      );
+      Response response = await dio.get(
+        ApiEndpoints.verifyUser,
+        options: Options(
+          headers: {'Authorization': 'Bearer $token'},
+        ),
       );
       if (response.statusCode == 200) {
-        String token = response.data["token"];
-        // Save token to shared prefs
-        await userSharedPrefs.setUserToken(token);
+        return const Right(true);
+      }
+      return Left(
+        Failure(
+            error: response.data['message'],
+            statusCode: response.statusCode.toString()),
+      );
+    } on DioException catch (e) {
+      return Left(Failure(error: e.error.toString()));
+    }
+  }
+
+  Future<Either<Failure, AuthEntity>> getMe() async {
+    try {
+      // Fetch token
+      String? token;
+      final data = await userSharedPrefs.getUserToken();
+      data.fold(
+        (l) => token = null,
+        (r) => token = r,
+      );
+
+      // Fetch user data from API
+      Response response = await dio.get(
+        ApiEndpoints.getMe,
+        options: Options(
+          headers: {'Authorization': 'Bearer $token'},
+        ),
+      );
+
+      // Print for debugging
+      print(response.data); // Ensure this matches your expected model
+
+      if (response.statusCode == 200) {
+        // Adjust parsing to ensure type correctness
+        final userData = response.data['data'];
+        if (userData != null) {
+          // Convert to AuthEntity
+          final authEntity = AuthApiModel.fromJson(userData).toEntity();
+          return Right(authEntity);
+        } else {
+          return Left(Failure(error: 'User data is missing in the response'));
+        }
+      }
+      return Left(
+        Failure(
+          error: response.data['message'],
+          statusCode: response.statusCode.toString(),
+        ),
+      );
+    } on DioException catch (e) {
+      return Left(Failure(error: e.error.toString()));
+    }
+  }
+
+  Future<Either<Failure, bool>> updateUser(AuthEntity updateUser) async {
+    try {
+      String? token;
+      var data = await userSharedPrefs.getUserToken();
+      data.fold(
+        (l) => token = null,
+        (r) => token = r,
+      );
+
+      if (token == null) {
+        return Left(
+          Failure(
+            error: 'No token found',
+          ),
+        );
+      }
+
+      Response response = await dio.put(
+        ApiEndpoints.updateUser,
+        data: authApiModel.fromEntity(updateUser).toJson(),
+        options: Options(
+          headers: {
+            'Authorization': 'Bearer $token',
+          },
+        ),
+      );
+
+      if (response.statusCode == 200) {
         return const Right(true);
       } else {
         return Left(
           Failure(
-            error: response.data["message"],
+            error: response.data['message'],
             statusCode: response.statusCode.toString(),
           ),
         );
